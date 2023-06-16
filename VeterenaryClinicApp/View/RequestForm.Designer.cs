1dae19547bb63810806e57b864dc21b26a8a1de1
McConderEz <kamazitun.jaykehd@gmail.com>
2023-06-16 10:04:21 +0300

    Added 2/3 request from TZ

--- VeterenaryClinicApp/View/RequestForm.Designer.cs
+++ VeterenaryClinicApp/View/RequestForm.Designer.cs
@@ -78,6 +78,7 @@
             this.button2.TabIndex = 10;
             this.button2.Text = "Запрос 3";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
